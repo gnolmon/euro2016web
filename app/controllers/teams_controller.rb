9e32aa343e771af010bbfaafe7c5afc5c875f970
gnolmon <gnolmon@gmail.com>
2016-05-17 18:03:44 +0700

    asc group

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,12 +4,13 @@ class TeamsController < ApplicationController
   # GET /teams
   # GET /teams.json
   def index
-    @teams = Team.all
+    @teams = Team.all.order(group: :asc, point: :desc)
     respond_to do |format|
       format.html
       format.json { render json: @teams }
     end
   end
+
 
   # GET /teams/1
   # GET /teams/1.json
